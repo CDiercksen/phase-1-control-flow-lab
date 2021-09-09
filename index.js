aa282f6b238b854d11c27c06bf131f45501faf13
Christopher <christopher.diercksen@gmail.com>
2021-09-09 17:48:20 -0400

    commit tree e9697fa0d337ae1728ab9243995afb5d9d25e71c

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!'
+  }
+  else if (someValue >= 2000 &&  someValue < 2500) {
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue >= 2500) {
+    result = 'No can do.'
+  }
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someCity){
   // Write your code here!
+  return someCity === 'NYC'?`Ok, sounds good.`:`No go.`
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  let charm;
+
+  switch (someTip) {
+    case 'generous':
+      charm = "Thank you so much.";
+      break;
+    case 'not as generous':
+      charm = 'Thank you.';
+      break;
+    default:
+      charm = 'Bye.';
+  }
+return charm
 }
